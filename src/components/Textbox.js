9ee0ec4874733c87f1698da78818555216ac4513
Shane Segui <shanesegui@Shanes-iMac.local>
2021-07-02 17:33:43 +0900

    update textbox style

--- src/components/Textbox.js
+++ src/components/Textbox.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function Textbox({ value, placeholder = "", onChange }) {
     return (
-        <input className="outline-none rounded-md border border-gray-500 dark:border-gray-300 py-2 px-2 appearance-none text-base shadow-md text-gray-500" 
+        <input className="outline-none rounded-md border border-gray-500 dark:border-gray-300 py-2 px-2 appearance-none text-base text-gray-800" 
         type="text" 
         value={value} onChange={onChange} placeholder={placeholder} />
     )
